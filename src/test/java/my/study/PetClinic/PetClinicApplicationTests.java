d52ac8d32a7cd80cf9f75fcf7edfc117cdacd7e5
Volodymyr <shvachkovol@gmail.com>
2021-07-04 13:43:49 +0300

    Initialized with start.spring.io

---
+++ src/test/java/my/study/PetClinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package my.study.PetClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
